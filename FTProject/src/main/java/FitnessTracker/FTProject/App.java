8c617275ceb7c0a0276e3a74df77905bb1102263
ECS\Terlizzi <Garth_Terlizzi@Baylor.edu>
2018-03-22 22:04:23 -0500

    Initial Commit

---
+++ FTProject/src/main/java/FitnessTracker/FTProject/App.java
@@ -0,0 +1,13 @@
+package FitnessTracker.FTProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
