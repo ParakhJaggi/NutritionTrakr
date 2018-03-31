ff17735c4ce6f1a9864a68eb0c9276d86659ed49
Parakh Jaggi <parakh_jaggi@baylor.edu>
2018-03-31 16:31:30 -0500

    implemented pressregister that adds user to database once user types in info

--- FTProject/src/main/java/FitnessTracker/FTProject/RegisterUserController.java
+++ FTProject/src/main/java/FitnessTracker/FTProject/RegisterUserController.java
@@ -1,5 +1,7 @@
 package FitnessTracker.FTProject;
 
+import java.sql.SQLException;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -20,16 +22,41 @@ public class RegisterUserController {
 	@FXML
 	public Button registerButton;
 	@FXML
-	public TextField hight;
+	public TextField height;
 	@FXML
 	public TextField weight;
 	@FXML
 	public TextField waistMeasurement;
 	@FXML
 	public TextField neckMeasurement;
+	@FXML
+	public TextField firstname;
+	@FXML
+	public TextField lastname;
 	
 	@FXML 
-	private void PressRegister() {
+	private void PressRegister() throws SQLException {
+		User usr;
+		if(genderbox.getValue().equals("Male")) {
+			usr = new MaleUser();
+		}
+		else {
+			usr = new FemaleUser();
+		}
+		usr.setEmail(email.getText());
+		usr.setFirstName(firstname.getText());
+		usr.setLastName(lastname.getText());
+		usr.setGender(genderbox.getValue());
+		usr.setHeight(Double.parseDouble(height.getText()));
+		usr.setNeckMeasurment(Double.parseDouble(neckMeasurement.getText()));
+		usr.setPassword(password.getText());
+		usr.setWaistMeasurment(Double.parseDouble(waistMeasurement.getText()));
+		usr.setWeight(Double.parseDouble(weight.getText()));
+		//usr.setUserId(i);
+		
+		DatabaseGateway d = new DatabaseGateway();
+		d.registrationHelper(usr);
+		
 		
 	}
 	@FXML
