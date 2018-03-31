82c9b25f62af2af960ff36884fa4d05e4b2c9f6a
Parakh Jaggi <parakh_jaggi@baylor.edu>
2018-03-30 19:05:21 -0500

    Added username and password fields for register page

--- FTProject/src/main/java/FitnessTracker/FTProject/RegisterUserController.java
+++ FTProject/src/main/java/FitnessTracker/FTProject/RegisterUserController.java
@@ -4,11 +4,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class RegisterUserController {
 	@FXML
 	public ChoiceBox<String> genderbox;
+	@FXML
 	ObservableList<String> GenderList = FXCollections.observableArrayList("Male","Female");
+	@FXML
+	public TextField username;
+	@FXML 
+	public PasswordField password;
+	
 	
 	@FXML
 	private void initialize() {
