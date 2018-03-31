674e3b3179a6638bc3323835b3bd821b9cf536b8
Parakh Jaggi <parakh_jaggi@baylor.edu>
2018-03-31 15:13:02 -0500

    pressing sign up will open register page

--- FTProject/src/main/java/FitnessTracker/FTProject/Controller.java
+++ FTProject/src/main/java/FitnessTracker/FTProject/Controller.java
@@ -1,11 +1,17 @@
 package FitnessTracker.FTProject;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Controller {
 	@FXML
@@ -13,9 +19,18 @@ public class Controller {
 	@FXML
 	public PasswordField pass;
 	
-	public void pressSignUp(ActionEvent action) {
+	public void pressSignUp(ActionEvent action) throws IOException {
+		Parent root;
+        
+        //root = FXMLLoader.load(getClass().getClassLoader().getResource("signup.fxml"));
+		root = FXMLLoader.load(getClass().getResource("signup.fxml"));
 		
-		
+        Stage stage = new Stage();
+        stage.setTitle("My New Stage Title");
+        stage.setScene(new Scene(root, 450, 450));
+        stage.show();
+        // Hide this current window (if this is what you want)
+         ((Node)(action.getSource())).getScene().getWindow().hide();
 	}
 	public void pressSignIn(ActionEvent action) throws SQLException {
 		
