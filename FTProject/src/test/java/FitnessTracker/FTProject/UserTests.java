7fb88a056c58f76b0a54addce46715b6d0afbc32
ECS\Terlizzi <Garth_Terlizzi@Baylor.edu>
2018-04-21 01:29:21 -0500

    New Test class

---
+++ FTProject/src/test/java/FitnessTracker/FTProject/UserTests.java
@@ -0,0 +1,22 @@
+package FitnessTracker.FTProject;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import junit.framework.TestCase;
+
+public class UserTests extends TestCase {
+	//TODO: once Parakh makes User a singleton
+	/*
+	@BeforeAll
+	public void create() {
+		MaleUser m=new MaleUser();
+		FemaleUser f=new FemaleUser();
+	}
+	
+	@Test
+	public void verifyBodyFatMale() {
+		
+	}
+	
+*/
+}
